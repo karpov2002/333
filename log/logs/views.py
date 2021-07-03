02f257e9ac8b55c687154945227cc504923f6640
Student <Student@kpk.local>
2021-07-03 12:27:12 +0500

    rrr

---
+++ log/logs/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from .models import Topic
+
+
+def index(request):
+    topics = Topic.objects.order_by('-date_added')
+    context = {
+        'topics': topics,
+    }
+    return render(request,'logs/index.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {
+        'topic': topic,
+        'entries': entries,
+    }
+    return render(request, 'logs/topic.html', context)
